fba5cb28a6aa1396b2affa45737d560653a2d68f
Michael Esses <messes@umich.edu>
2014-09-06 10:58:29 -0400

    Implemented Random Selection and Send Text Messages

--- app/src/main/java/com/messes/panorama/bang/BangActivity.java
+++ app/src/main/java/com/messes/panorama/bang/BangActivity.java
@@ -1,8 +1,12 @@
 package com.messes.panorama.bang;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.PendingIntent;
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,21 +14,24 @@ import android.util.Log;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class BangActivity extends Activity {
 
-    ArrayList<String> contacts;
+    ContentResolver cr;
+    ArrayList<Integer> contacts;
+    Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bang);
 
-        contacts = new ArrayList<String>();
+        contacts = new ArrayList<Integer>();
 
-        ContentResolver cr = getContentResolver();
-        Cursor cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        cr = getContentResolver();
+        cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
 
         while (cursor.moveToNext()) {
             String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
@@ -41,7 +48,7 @@ public class BangActivity extends Activity {
                         break;
                     case ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE:
                         // do something with the Mobile number here...
-                        contacts.add(number.replaceAll("[^\\d.]", ""));
+                        contacts.add(cursor.getPosition());
                         break;
                     case ContactsContract.CommonDataKinds.Phone.TYPE_WORK:
                         // do something with the Work number here...
@@ -50,7 +57,6 @@ public class BangActivity extends Activity {
             }
             phones.close();
         }
-        cursor.close();
     }
 
 
@@ -74,12 +80,49 @@ public class BangActivity extends Activity {
     }
 
     public void bang(View view) {
-        Log.i("BangActivity", "Successful button click");
+        Random r = new Random();
+        Integer idx = r.nextInt(contacts.size());
 
-        for (int i = 0; i < contacts.size(); i++) {
-            Log.i("BangActivity", contacts.get(i));
+        if (!cursor.moveToPosition(contacts.get(idx))) {
+            return;
         }
 
-        Log.i("BangActivity", "bang() statements did not throw exception");
+        String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+        Cursor phones = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId, null, null);
+
+        while (phones.moveToNext()) {
+            String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+            String number = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            number = number.replaceAll("[^\\d.]", "");
+            int type = phones.getInt(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
+            switch (type) {
+                case ContactsContract.CommonDataKinds.Phone.TYPE_HOME:
+                    // do something with the Home number here...
+                    break;
+                case ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE:
+                    // do something with the Mobile number here...
+                    sendMessage(name + " " + number, "2032463012");
+                    Log.i("BangActivity", "sendMessage called on number " + number);
+                    break;
+                case ContactsContract.CommonDataKinds.Phone.TYPE_WORK:
+                    // do something with the Work number here...
+                    break;
+            }
+        }
+        phones.close();
+    }
+
+    private void sendMessage(String message, String number) {
+        try {
+            PendingIntent pi = PendingIntent.getBroadcast(BangActivity.this, 0, new Intent("SMS_SENT"), 0);
+            SmsManager.getDefault().sendTextMessage(number,null, message, pi, null);
+            Log.i("BangActivity", "sendMessage end of try block");
+        } catch (Exception e) {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(BangActivity.this);
+            AlertDialog dialog = alertDialogBuilder.create();
+            dialog.setMessage(e.getMessage());
+            dialog.show();
+        }
     }
 }
